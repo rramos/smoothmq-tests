4326b1210a56c6fec465717cad9be5676756fbfd
Rui Ramos <rui.ms.ramos@gmail.com>
2024-07-03 22:05:01 +0100

    updated defaul WAS credentials

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import boto3
 
 sqs = boto3.client(
     'sqs',
-    aws_access_key_id="YOUR_ACCESS_KEY_ID",
-    aws_secret_access_key="YOUR_SECRET_ACCESS_KEY",
+    aws_access_key_id="DEV_ACCESS_KEY_ID",
+    aws_secret_access_key="DEV_SECRET_ACCESS_KEY",
     endpoint_url='http://localhost:3001'
 )
 
